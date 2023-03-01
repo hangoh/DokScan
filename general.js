c49d6380b0084803d6a15c77a8154e76a78886b0
YU HAN GOH <gohyuhan12345@gmail.com>
2023-03-02 01:48:50 +0900

    Add files via upload

--- general.js
+++ general.js
@@ -58,51 +58,21 @@
         ///        close_pop_up_screen();
         ///    }
         ///});
-        function getCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-            }
-            return null;
-        }
-
-        function eraseCookie(name) {   
-            document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        }
-
-        function setCookie(name,value,days) {
-            var expires = "";
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime() + (days*24*60*60*1000));
-                expires = "; expires=" + date.toUTCString();
-            }
-            document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-        }
+        
 
         function get_csrf(){
-            var csrf_t = ""
-            $.ajax({
-                url: "http://127.0.0.1:8000/api/",
-                type: "GET",
-                credentials: 'include',
-                success: function(response) {
-                    // Image data received from backend API
-                    setCookie('csrftoken',response.token,7);
-                    console.log(response.headers)
-
-                // Do your OpenCV processing here...
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                console.log("AJAX error: " + textStatus + " - " + errorThrown);
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://127.0.0.1:8000/api/');
+            xhr.send();
+            xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                    const headersString = xhr.getAllResponseHeaders();
+                    return headersString['x-csrftoken']
+                    // Do something with the headers string
                 }
-            })
-            return csrf_t
+            };
         }
 
-        get_csrf()
+        
        
         
