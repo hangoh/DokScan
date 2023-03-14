645bb6c196e85c7cfde6008cb0ff374c9297a194
hangoh <gohyuhan12345@gmail.com>
2023-03-14 16:30:11 +0900

    updated loading mechanic

--- general.js
+++ general.js
@@ -64,8 +64,9 @@ confirmBtn.addEventListener('click', function() {
     // and then close the modal dialog box
     console.log(points)
     console.log(tem_points)
-    close_pop_up_screen();
     form_doc_data(data)
+    close_pop_up_screen();
+    
     
 });
 
@@ -225,94 +226,7 @@ function form_doc_data(imageData){
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log("AJAX error: " + textStatus + " - " + errorThrown);
-            forming_doc = false
             
         }
     });
-    
-}
-
-function loading(){
-    const canvas = document.getElementById('loading')
-    document.getElementById('loading_modal').style.display = 'block'
-    
-        const canvasWidth = 80;
-        const canvasHeight = 24;
-        const canvasArea = canvasHeight * canvasWidth;
-        const yOffset = 12;
-        const xOffset = 40;
-        const innerRadius = 2;
-        const r1Points = 90; // 90
-        const r2Points = 314; // 314
-        const fov = 5;
-
-        const what = 30;
-
-        let A = 0;
-        let B = 0;
-
-        let shades = '.,-~:;=!*#$@'.split('');
-        // let shades = 'patrick PATRICK'.split('');
-
-        // buffers
-        let b, z;
-        var interval = false
-        interval = setInterval(() => {
-            if(forming_doc){
-                b = Array(canvasArea).fill(' '); //
-                z = Array(7040).fill(0); // z-buffer set to z^-1
-
-                for (let j = 0; j < 6.28; j += 6.28 / r1Points) {
-                for (let i = 0; i < 6.28; i += 6.28 / r2Points) {
-                    let c = Math.sin(i);
-                    let d = Math.cos(j);
-                    let e = Math.sin(A);
-                    let f = Math.sin(j);
-                    let g = Math.cos(A);
-
-                    let h = d + innerRadius;
-
-                    let D = 1 / (c * h * e + f * g + fov);
-
-                    let l = Math.cos(i);
-                    let m = Math.cos(B);
-                    let n = Math.sin(B);
-                    let t = c * h * g - f * e;
-
-                    // floored floats a.k.a. ints
-                    let x = (xOffset + what * D * (l * h * m - t * n)) << 0;
-                    let y = (yOffset + (what / 2) * D * (l * h * n + t * m)) << 0;
-                    let o = (x + canvasWidth * y) << 0;
-                    let shadeConstant = (((shades.length + 1) * 2) / 3) << 0; // ceil(shade.length * (2/3))
-                    let N =
-                    (shadeConstant *
-                        ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n)) <<
-                    0;
-
-                    if (canvasHeight > y && y > 0 && x > 0 && canvasWidth > x && D > z[o]) {
-                    z[o] = D;
-                    b[o] = shades[N > 0 ? N : 0];
-                    }
-                }
-                }
-
-                canvas.innerHTML = '';
-                let line = [];
-
-                for (let k = 0; k < canvasArea + 1; k++) {
-                if (k % canvasWidth) {
-                    line.push(b[k]);
-                } else {
-                    canvas.innerHTML += line.join('') + '<br />';
-                    line = [];
-                }
-
-                A += 0.00002;
-                B += 0.00001;
-                }
-            }else{
-                document.getElementById('loading_modal').style.display = 'none'
-                
-            }
-        }, 17);
 }
